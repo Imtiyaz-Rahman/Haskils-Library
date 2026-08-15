68dabb75926151a3a7e11378080242f0d44bfbfe
Imtiyaz Rahman <Imtiyaz@Rahman.ibm.com>
2026-08-15 15:44:23 +0100

    resolving base path issue

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/haskils-library",
+  assetPrefix: "/haskils-library/",
   /* config options here */
 };
 
