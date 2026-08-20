063bcc00d656eab82745166f3dcdeaee5b6a7fe8
Imtiyaz-Rahman <imtiyazr@outlook.com>
2026-08-21 00:48:25 +0100

    fixed sizing and link

--- app/dashboard/projects/layout.tsx
+++ app/dashboard/projects/layout.tsx
@@ -10,7 +10,7 @@ export default function ProjectLayout({
       <NavbarComponent />
       <div className="min-h-svh flex bg-prussian justify-center">
         <div className="min-h-svh flex mt-3 p-6 content-center bg-alabaster w-6/10 rounded-md">
-          <article className="prose w-full py-4 px-8 md:w-4/5 lg:w-3/5 text-alabaster">
+          <article className="prose w-full py-4 px-8 w-full text-alabaster">
             {children}
           </article>
         </div>
